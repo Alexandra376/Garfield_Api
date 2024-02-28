dcdbe4cbf57ae5ff3d824a1e1b51e1d3fe75dd79
Alexandra376 <suvorovaalexandra344@gmail.com>
2024-02-28 21:50:50 +0300

    feat: add styles

--- HOMEWORK/script.js
+++ HOMEWORK/script.js
@@ -8,12 +8,11 @@ const filmOscar = document.getElementById("oscar");
 async function getData() {
     const res = await fetch("https://alexandra376.github.io/Garfield_Api/FAKEAPI/data.json");
     const data = await res.json();
-    const {title, year, director, poster, description, hasOscar} = data
+    const {title, year, poster, description, director, hasOscar} = data
     filmTitle.textContent = `${title}, ${year} year`;
     filmOscar.textContent = hasOscar ? "This film won oscar" : "No awards";
     filmPoster.src = poster;
     filmDescription.textContent = description;
     filmDirector.textContent = director;
-    filmYear.textContent = year;
 }
 getData();
