983a0a4a6c127171beedc206a970316916856f4e
Alexandra376 <suvorovaalexandra344@gmail.com>
2024-02-28 21:35:24 +0300

    feat: add fetch response

---
+++ HOMEWORK/script.js
@@ -0,0 +1,19 @@
+const filmTitle = document.getElementById("title");
+const filmYear = document.getElementById("year");
+const filmDirector = document.getElementById("director");
+const filmDescription = document.getElementById("description")
+const filmPoster = document.getElementById("poster");
+const filmOscar = document.getElementById("oscar");
+
+async function getData() {
+    const res = await fetch("https://alexandra376.github.io/Garfield_Api/FAKEAPI/data.json");
+    const data = await res.json();
+    const {title, year, director, poster, description, hasOscar} = data
+    filmTitle.textContent = `${title}, ${year} year`;
+    filmOscar.textContent = hasOscar ? "This film won oscar" : "No awards";
+    filmPoster.src = poster;
+    filmDescription.textContent = description;
+    filmDirector.textContent = director;
+    filmYear.textContent = year;
+}
+getData();
